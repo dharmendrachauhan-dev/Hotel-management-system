e7b5a31e1b49fa3e6cd532509cb27bb42fcbecfe
Dharmendra Chauhan <dmma359800@gmail.com>
2026-06-05 14:36:41 +0530

    Refactor room model schema to include amenities and images fields

--- backend/src/models/rooms.models.js
+++ backend/src/models/rooms.models.js
@@ -1,3 +1,41 @@
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
-const 
+const roomSchema = new Schema(
+    {
+        roomNumber: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        roomType: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        capacity: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+            default: "This is clean and ready to use top class room for your family"
+        },
+        amenities: {
+            type: String
+        },
+        images: {
+            type: String,
+        },
+        isAvailable: {
+            type: Boolean,
+            required: true
+        },
+    },
+    {timestamps: true}
+)
+
+export const Room = new model("Room", roomSchema)
