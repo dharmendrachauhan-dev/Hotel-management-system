19648c6de3ccdd3e3cb4a944dd9cea93ac8de034
Dharmendra Chauhan <dmma359800@gmail.com>
2026-06-05 15:51:14 +0530

    improve validation in review and fix some bugs

--- backend/src/models/payments.models.js
+++ backend/src/models/payments.models.js
@@ -4,31 +4,37 @@ const paymentSchema = new Schema(
     {
         booking: {
             type: Schema.Types.ObjectId,
-            ref: "Booking"
+            ref: "Booking",
+            required: true,
         },
         amount: {
             type: Number,
-            required: true
+            required: true,
+            min: 0, // this needed so amount dont go in negative
         },
         paymentMethod: {
             type: String,
-            required: true
+            required: true,
+            enum: ["Card", "UPI", "Cash", "Net Banking"],// I want here people get only this option or nothing else
         },
         transactionId: {
             type: String,
             unique: true,
-            required: true
+            required: true,
+            trim: true,
         },
         status: {
             type: String,
-            required: true
+            required: true,
+            enum: ["Pending", "Completed", "Failed", "Refunded"],
         },
         paidAt: {
             type: Date,
-            required: true
+            default: Date.now,
         }
-    }
+    },
+    {timestamps: true}
 )
 
-export const Payment = new model("Payment", paymentSchema)
+export const Payment = model("Payment", paymentSchema)
 
