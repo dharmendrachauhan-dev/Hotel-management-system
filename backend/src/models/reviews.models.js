74dcb77bea7e2a2de688aedaf0e34184061a1e21
Dharmendra Chauhan <dmma359800@gmail.com>
2026-06-05 15:39:48 +0530

    fix review model bugs

--- backend/src/models/reviews.models.js
+++ backend/src/models/reviews.models.js
@@ -4,20 +4,26 @@ const reviewSchema = new Schema(
     {
         user:{
             type: Schema.Types.ObjectId,
-            ref: "User"
+            ref: "User",
+            required: true
         },
         room: {
             type: Schema.Types.ObjectId,
-            ref: "Room"
+            ref: "Room",
+            required: true
         },
         rating: {
-            type: Number
+            type: Number,  // rating should be decided 1 to 5 or 1 to 10
+            min: 1,
+            max: 5,
+            required: true
         },
         comment: {
-            type: String
+            type: String,
+            trim: true,
         }
     },
     {timestamps: true}
 )
 
-export const Review = new model("Review", reviewSchema)
+export const Review = model("Review", reviewSchema)
