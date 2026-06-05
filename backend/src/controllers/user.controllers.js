631f2c126c614a5937c0d8831d4614ccb7462483
Dharmendra Chauhan <dmma359800@gmail.com>
2026-06-05 17:49:54 +0530

    add generate and refresh token

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,12 +1,34 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiRespose.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiRespose.js"
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
 import { User } from "../models/users.models.js"
 
 
-const registerUser = asyncHandler(async (req, res)=> {
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        if (!userId) {
+            throw new ApiError(400, "UserId is required")
+        }
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new ApiError(400, "User not found")
+        }
+
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { refreshToken, accessToken }
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+const registerUser = asyncHandler(async (req, res) => {
     res.json("Hi this is register")
 })
 
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,3 +1,29 @@
-// import { ApiError } from "../utils/ApiError.js"
-// import { asyncHandler } from "../utils/asyncHandler.js"
-// import jwt from "jsonwebtoken"
+import { User } from "../models/users.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
+
+
+export const verifyJWT = asyncHandler(async (req, res) => {
+    try {
+        const token = req.cookies?.accessToken || 
+        req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token){
+            throw new ApiError(400, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log("verify decoded token => ", decodedToken)
+    
+        const user = await User.findById(decodedToken?._id).select('-password -refreshToken')
+        if(!user){
+            throw new ApiError(401, "Invalid access token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
